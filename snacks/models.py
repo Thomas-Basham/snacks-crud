1875169704ee9659acdcb7c994fb274df6346d2a
Thomas Basham <Bashamtg@gmail.com>
2022-05-25 10:54:02 -0700

    admin setup, model created

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+
+
 # Create your models here.
+class Snack(models.Model):
+    name = models.CharField(max_length=64)
+    purchaser = models.ForeignKey(get_user_model(), models.CASCADE)
+    description = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+# import django_heroku
+# import dj_database_url
+# import dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'snacks',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +59,9 @@ ROOT_URLCONF = 'snacks_crud_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static/"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
